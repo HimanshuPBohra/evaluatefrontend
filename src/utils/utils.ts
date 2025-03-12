2c61853fd1b2632dfba2bf7f55f0a339f47bd7c6
HimanshuPBohra <bohrah467@gmail.com>
2025-03-12 15:59:49 +0530

    alibaba

---
+++ src/utils/utils.ts
@@ -0,0 +1,9 @@
+const serverURL = "http://localhost:8080"
+const hostname = "https://arham.ai";
+const currencySymbol = "₹";
+const appName = "EvaluateAI - A Arham.ai Product";
+//UploadThing
+const maxFileCount = 50;
+const maxFileSize = "128MB"; 
+
+export { serverURL, currencySymbol, appName, maxFileCount, maxFileSize, hostname };
